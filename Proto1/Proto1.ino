baf27becb0b6a7287c5eb25a1778fb8dc44bddff
Anotinas <guslindaaq@student.gu.se>
2018-04-12 15:54:50 +0200

    Changed go and rotate to setSpeed and setAngle.

--- Proto1/Proto1.ino
+++ Proto1/Proto1.ino
@@ -3,13 +3,12 @@
 Odometer encoderLeft, encoderRight;
 Gyroscope gyro;
 Car car;
-char action;
+Sonar frontSonar;
 
 const int TRIG_PIN = 6;
 const int ECHO_PIN = 5;
-Sonar frontSonar;
-int i = 0;
 char mode;
+char action;
 
 
 void setup() {
@@ -27,120 +26,54 @@ void setup() {
 
 void loop() {
 
-  /* manual_mode();
-  }
-
-void manual_mode(){*/
     while(Serial.available() > 0){
     //int action = interpretInput(Serial.read());
 
     action = Serial.read();
-    switch(action){
+    manual_mode();
+  }
+}
+
+void manual_mode(){
   
+    switch(action){
     case 'f' : //Forward 
       moveDstyle(3);
       
     case 'b' : //Backwards
-      moveDstyle(-3);
+      moveDstyle(-5);
 
-    case 'l' : // rotate left
-      rotateDstyle(3);
-      
     case 'r' : // rotate right
-      rotateDstyle(-3);
+      rotateDstyle(5);
+      
+    case 'l' : // rotate left
+      rotateDstyle(-5);
 
       case 'm' :// mode change
       //Should call the autonomous mode function.
-      break;
-
-    /*case 5 // forward, rotate right
-      move_rotate(int moveDir, int rotateDir);
-      break;
-
-    case 6 // forward, rotate left
-      move_rotate(int moveDir, int rotateDir);
-      break;
-
-    case 7 // back, rotate right
-      move_rotate(int moveDir, int rotateDir);
-      break;
-
-    case 8 // back, rotate left
-      move_rotate(int moveDir, int rotateDir);
-      break;
-      */
-    
+      break; 
   }
-  }
+  
 }
-
-  /*int interpretInput(String input){
-    if(input == "m"){
-      return 9;
-    }
-    
-    else if(input.charAt(0) == "f"){// move forward++;
-      if(input == "f"){ //move forward
-        return 1;
-      }
-      
-      else if(input == "fr"){ // rotate right and go forward
-        return 5;
-      }
-
-      else if(input == "fl"){// rotate left and go forward
-        return 6;
-      }
-      
-    }
-
-    else if(input.charAt(0) == "b"){ // move backwards++;
-      if(input =="b"){ // move back.
-        return 2;
-      }
-
-      else if(input == "br"){ // rotate right and go back
-        return 7;
-      }
-
-      else if(input == "bl"){ // rotate left and go back
-
-        return 8;
-      }
-    }
-
-    else if(input == "l"){ // Rotate left.
-      return 3;
-    }
-    
-    else if(input == "r"){ // Rotate right.
-      return 4;
-    }
-  }*/
   
 
   void moveDstyle(int dir){
     if(NoCrashDstyle() && dir > 0){
-    car.go(dir);
+    car.setSpeed(50);
     }
    else {
-      car.go(dir);
+      car.setSpeed(-50);
     }
   }
 
   void rotateDstyle(int dir){
-    car.rotate(dir);
-  }
-  void move_rotate(int moveDir, int rotateDir){
-    if(NoCrashDstyle()){
-    car.go(moveDir);
-    }
-    car.rotate(rotateDir);
+    car.setAngle(dir);
   }
   
   boolean NoCrashDstyle(){ //Experimental
   if(frontSonar.getDistance() > 0 && frontSonar.getDistance()< 15){
     return false;
+    car.stop;
   }
   return true;
 }
